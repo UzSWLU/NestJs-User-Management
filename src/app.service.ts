21b99cc3494e71c45edf533a99090db0a5b84e7c
a-d-sh <ad.sh.1997@mail.ru>
2025-10-14 18:04:46 +0500

    NestJS + MySQL + PhpMyAdmin

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const appName = process.env.APP_NAME || 'Nest App';
+    return `Hello from ${appName}!`;
   }
 }
