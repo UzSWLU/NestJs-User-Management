152f64d52bee28bdcdf82e045cbe16a95274e2ab
a-d-sh <ad.sh.1997@mail.ru>
2025-10-14 18:30:07 +0500

    Swagger o'rnatildi

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.APP_PORT ?? 3000);
+
+  // Swagger konfiguratsiyasi
+  const config = new DocumentBuilder()
+    .setTitle('User Management API')
+    .setDescription('Management-as-a-Service platform API documentation')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  // createDocument qaytaradigan obyekt turi -> OpenAPIObject
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document);
+
+  await app.listen(3000);
+  console.log('🚀 Swagger running at: http://localhost:3000/');
 }
-bootstrap();
+
+// eslint xatosiz, xavfsiz usulda ishga tushirish
+void bootstrap().catch((err) => {
+  console.error('❌ NestJS bootstrap failed:', err);
+  process.exit(1);
+});
